6abc8a6352f20f56da6a4bfe5029bd369aeb2967
Anindya Tonoy Mazumder <38114687+TheStranger1@users.noreply.github.com>
2019-12-18 05:04:46 +0200

    Update question_1.py

--- Procedures and Functions/question_1.py
+++ Procedures and Functions/question_1.py
@@ -1,9 +1,15 @@
-def outputUnderlined(s):
-    print s
-    print len(s)*"-"
+#Write a procedure that gets two parameters, n and m, and proceeds to output a multiplication table with
+#values ( 1.n, 2.n...m.n)
+#Example output (with argument n == 5 and m == 3):
+#1 * 5 = 5
+#2 * 5 = 10
+#3 * 5 = 15
 
+def multi(n, m):
+    for i in range(1,m+1):
+        print (i*n)
 
-outputUnderlined("Hello")     
+multi(5,3)   
 
     
 

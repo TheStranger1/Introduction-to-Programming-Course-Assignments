d3372e1ce5f31f17b4b96bfa42d58870bdc6c227
Anindya Tonoy Mazumder <38114687+TheStranger1@users.noreply.github.com>
2021-08-11 20:51:53 +0300

    Update question_5.py

--- Repetitive Execution/question_5.py
+++ Repetitive Execution/question_5.py
@@ -1,7 +1,7 @@
 #String B is said to be a permutation of string A, if an equal amount of each character in A can be found in B
-#and vice versa. Hence, B is said to be a rearrangement of A. For example, strings “ABBA”, “BAAB” and
-#“ABAB” are all permutations of string “AABB”, and string “elvis” and “sevil” are permutations of string
-#“lives”. Note, that these string permutations are sometimes called anagrams.
+#and vice versa. Hence, B is said to be a rearrangement of A. For example, strings â€œABBAâ€, â€œBAABâ€ and
+#â€œABABâ€ are all permutations of string â€œAABBâ€, and string â€œelvisâ€ and â€œsevilâ€ are permutations of string
+#â€œlivesâ€. Note, that these string permutations are sometimes called anagrams.
 #Write a program that queries the user for strings A and B, and proceeds to output whether B is a
 #permutation of A. To confirm this, the following conditions need to be met:
 #1. There is an equal amount of characters in A and B (or, the length of A is equal to length of B), and
@@ -13,7 +13,7 @@ stringb=raw_input("Enter string B: ")
 perm=False #boolean variable initially set to false.
 
 for char in stringa: 
-    if char in stringb and len(stringa)==len(stringb): #check if for every character in stringa is present in #string b and if the length of both is equal
+    if char in stringb and set(stringa)==set(stringb): #check if for every character in stringa is present in #string b and if the length of both is equal
         perm=True #set perm to true if the condition is met
 while(perm):
     print stringb,"is a permutation of",stringa 
